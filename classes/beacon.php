2939086dd46bf96a0e2ff94f21cce50eed7b3f5c
ImDom <dominik.rask@gmail.com>
2010-10-12 21:59:44 +0200

    Removed old files, new coming up soon

--- classes/beacon.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Beacon extends Kohana_Beacon {  }
--- config/beacon.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') or die('No direct access allowed.');
-
-return array(
-    'api_host'    => 'api.beaconpush.com',
-    'api_version' => '1.0.0',
-    'api_key'     => '',
-    'secret_key'  => '',
-);
