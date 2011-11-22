2492cfa8c8fbc155c30896eaad7391b2372cbb21
Dominik Rask <dominik.rask@gmail.com>
2011-11-22 23:23:09 +0100

    Updated the "documentation comments" the jQuery Beaconpush plugin file

--- jquery.beaconpush.js
+++ jquery.beaconpush.js
@@ -1,5 +1,4 @@
 jQuery.extend({
-
     BeaconPush: {
         addEventListener: function(event, handler) {
             Beacon.listen(function(data) {
@@ -7,28 +6,17 @@ jQuery.extend({
                     return handler(data);
                 }
             });
-        },
-
-        // Ignore these if you dont know how to use jQuery Tmpl
-        append: function(list, tmpl, event) {
-            $(tmpl).tmpl(event.data).appendTo(list);
-        },
-
-        prepend: function(list, tmpl, event) {
-            $(tmpl).tmpl(event.data).prependTo(list);
         }
     }
-
 });
 
 
 /* ########## Usage #############
 * ### PHP
-* $beaconpush->send_to_channels(array('theBestChannel', 'foo'), 'newMessage', array('myMessage' => 'Hello everyone!'));
+* $beaconpush->send_to_channel("theBestChannel", "chat-message", array("message" => "Hello! This is my message"));
 *
 * ### JavaScript
-* $.BeaconPush('newMessage', function(event) {
-*     console.log('The message: ' + event.data.myMessage);
-*     console.log('The event: ' + event.name);
+* $.BeaconPush.addEventListener("chat-message", function(e) {
+*   console.log(e);
 * });
 */
